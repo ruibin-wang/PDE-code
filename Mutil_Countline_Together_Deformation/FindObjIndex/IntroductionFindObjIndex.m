02b453413475dd8769aa676d856c0b397fca939b
Ruibin Wang <32572025+swjtuRobin@users.noreply.github.com>
2021-11-09 01:09:40 +0000

    Add files via upload

---
+++ Mutil_Countline_Together_Deformation/FindObjIndex/IntroductionFindObjIndex.m
@@ -0,0 +1,143 @@
+%%  --------swjtu_robin@foxmail.com------------%%
+%   -------- Ribin  20171206   ------------%
+
+%%
+clear;close all; clc
+
+load FaceIndex.txt    %% extracted from obj file
+load vertices.txt        %% extracted from obj file
+load halfTrain.txt
+
+%%  精确位数
+halfTrain       =       eval(vpa(halfTrain, 5));
+vertices        =       eval(vpa(vertices,5));
+
+selectHalfTrainHead =       zeros(size(vertices,1), size(vertices,2));  %% 从原始数据中提取能用于obj生成的数据
+
+%%
+for indexI = 1:size(vertices, 1)
+    memberIndex       =      ismember(halfTrain, vertices(indexI, :));
+    
+    firstColumIndex   =      find(memberIndex(:,1) == 1);  %% 与第一列相匹配的索引
+    secondColumIndex  =      find(memberIndex(:,2) == 1);  %% 与第二列相匹配的索引
+    thirdColumIndex   =      find(memberIndex(:,3) == 1);  %% 与第三列相匹配的索引
+    
+    combineIndex      =      [firstColumIndex; secondColumIndex; thirdColumIndex];  %% 将所有索引放入一个矩阵
+    
+    %%%  循环判断，如果相同的索引出现多于两次，则匹配到索引
+    for indexJ = 1:length(combineIndex)
+        if length(find(combineIndex == combineIndex(indexJ))) > 2   %%% 由于精确度的缘由，只能取舍了
+            selectHalfTrainHead(indexI, :) = halfTrain(combineIndex(indexJ), :);
+            break
+        end
+    end
+end
+
+%%  处理没有匹配到的索引位置
+
+firstSeHaTrHeIndex  = find(selectHalfTrainHead(:,1) == 0);   %% 没有达到匹配条件的索引
+
+for indexI = 1:length(firstSeHaTrHeIndex)
+    
+    transIndex = find(halfTrain(:, 1) == vertices(firstSeHaTrHeIndex(indexI), 1));
+    
+    if isempty(transIndex)
+        transIndex = find(halfTrain(:, 2) == vertices(firstSeHaTrHeIndex(indexI), 2));
+        
+        if isempty(transIndex)
+            transIndex = find(halfTrain(:, 3) == vertices(firstSeHaTrHeIndex(indexI), 3));
+            selectHalfTrainHead(firstSeHaTrHeIndex(indexI), :) = halfTrain(transIndex(1), :);
+        end
+        selectHalfTrainHead(firstSeHaTrHeIndex(indexI), :) = halfTrain(transIndex(1), :);
+    else
+        selectHalfTrainHead(firstSeHaTrHeIndex(indexI), :) = halfTrain(transIndex(1), :);
+    end
+end
+
+
+%% 为了防止匹配的时候出现重的数据，进行数据内部的筛选，找出数据内部中相同的点的索引
+
+testPoints = selectHalfTrainHead;
+step = 1;
+
+
+for indexI = 1:length(selectHalfTrainHead)-1  %% 进行检索
+    for indexJ = (indexI + 1):length(selectHalfTrainHead)
+        if selectHalfTrainHead(indexI, :) == selectHalfTrainHead(indexJ, :)
+            transMatrix{step} = [indexI, indexJ];
+            step = step +1;
+        end
+    end
+end
+
+%%  对于重叠部分重新降低精确度，重新进行匹配
+
+transSelHaTrHe = eval(vpa(selectHalfTrainHead, 4));
+transObjPoints = eval(vpa(vertices, 4));
+
+
+for indexI = 1:length(transMatrix)
+    for indexJ = 1:2
+        if transObjPoints(transMatrix{indexI}(indexJ), :) == transSelHaTrHe(transMatrix{indexI}(indexJ),:)
+            continue
+        else
+            selectHalfTrainHead(transMatrix{indexI}(indexJ), :) = pi;
+        end
+    end
+end
+
+
+%% 继续进行筛选，对剩余的相同的点进行处理
+
+lastIndex      =    find(selectHalfTrainHead(:,1) == pi);
+transSelHaTrHe =    eval(vpa(halfTrain, 4));
+
+for indexI = 1:length(lastIndex)
+    for indexJ = 1:length(halfTrain)
+        if transObjPoints(lastIndex(indexI), :) == transSelHaTrHe(indexJ, :)
+            selectHalfTrainHead(lastIndex(indexI), :) = halfTrain(indexJ, :);
+        end
+    end
+end
+
+%% 继续降低精确度，重新计算
+selectPointIndex = zeros(length(selectHalfTrainHead),1);
+
+% transObjPoints(1098,:)
+% transObjPoints(1567,:)
+% 
+% find(halfTrain(:,1) == transObjPoints(1098,1))
+% find(halfTrain(:,3) == transObjPoints(1567,3))
+
+selectPointIndex(1098) = 2070;
+selectPointIndex(1567) = 666;
+
+
+
+%% 根据匹配到的点，重构索引
+
+for indexI = 1:length(selectHalfTrainHead)
+    for indexJ = 1:length(halfTrain)
+        if selectHalfTrainHead(indexI,:) == halfTrain(indexJ, :)
+            selectPointIndex(indexI) = indexJ;
+        end
+    end
+end
+
+
+FaceIndex = FaceIndex - 2105*ones(length(FaceIndex),3);
+
+save selectPointIndex.mat selectPointIndex
+save FaceIndex.mat FaceIndex
+
+
+hold on; trisurf(FaceIndex, halfTrain(selectPointIndex, 1), halfTrain(selectPointIndex, 2), halfTrain(selectPointIndex, 3))
+axis equal
+% hold on; scatter3(selectHalfTrainHead(:,1), selectHalfTrainHead(:,2), selectHalfTrainHead(:,3), 'filled')
+% xlabel('X')
+% ylabel('Y')
+% zlabel('Z')
+
+% find(selectHalfTrainHead(:,1) == pi)
+
+% find(selectPointIndex(:,1) == 0)
---
+++ Mutil_Countline_Together_Deformation/findTrainHeadIndex/findTrainHeadPointIndex.m
@@ -0,0 +1,28 @@
+clear; clc
+
+load trainHead.txt
+load trainHeadFaceIndex.txt
+load vertTrainHead.txt
+
+transTrainHead          =     eval(vpa(trainHead, 4));
+transVertTrainHead      =     eval(vpa(vertTrainHead, 4));
+
+selectTrainHeadIndex    =     zeros(length(transVertTrainHead), 1);
+
+for indexI = 1:length(transVertTrainHead)
+    for indexJ = 1:length(transTrainHead)
+        if transVertTrainHead(indexI, :) == transTrainHead(indexJ, :)
+             selectTrainHeadIndex(indexI) = indexJ;
+        end
+    end
+end
+
+selectTrainHeadIndex(selectTrainHeadIndex == 0, :) = 2374;
+
+selectTrainHead  =  transTrainHead(selectTrainHeadIndex, :);
+
+hold on; trisurf(trainHeadFaceIndex, selectTrainHead(:,1), selectTrainHead(:,2), selectTrainHead(:,3))
+axis equal
+
+save selectTrainHeadIndex.mat selectTrainHeadIndex
+save trainHeadFaceIndex.mat trainHeadFaceIndex
